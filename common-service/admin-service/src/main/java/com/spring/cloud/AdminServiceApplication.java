7f1155d2c1553a1c8b5fde2303073c378d28c166
fenglij <feng_lj_89@163.com>
2018-08-24 16:38:14 +0800

    spring admin 整合配置

---
+++ common-service/admin-service/src/main/java/com/spring/cloud/AdminServiceApplication.java
@@ -0,0 +1,15 @@
+package com.spring.cloud;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class, args);
+    }
+}

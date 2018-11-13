b1198e538a196ca092b21e09b4853e13fc4bfa5d
fenglij <feng_lj_89@163.com>
2018-11-13 17:55:43 +0800

    添加spring cloud gateway 模块

---
+++ common-service/gateway-service/src/main/java/com/spring/cloud/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}

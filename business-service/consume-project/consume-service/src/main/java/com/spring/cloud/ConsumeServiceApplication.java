4c3b833c812de5f76a205213a1076fa5f33445f9
fenglij <feng_lj_89@163.com>
2018-11-14 10:32:08 +0800

    修改工程pom结构

--- business-service/consume-project/consume-service/src/main/java/com/spring/cloud/ConsumeServiceApplication.java
+++ business-service/consume-project/consume-service/src/main/java/com/spring/cloud/ConsumeServiceApplication.java
@@ -3,12 +3,18 @@ package com.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * exclude: 禁用指定的自动化配置来避免加载不必要的自动化配置
+ * 比如不需要加载数据源. 则 exclude: DataSourceAutoConfiguration.class
+ */
 @SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        DataSourceAutoConfiguration.class
 })
 @EnableDiscoveryClient
 @EnableFeignClients

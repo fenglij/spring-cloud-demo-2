7f0c007fc9f7b1b844a023a30d06cb271a821553
fenglj <abcd@1234>
2022-11-08 16:19:28 +0800

    添加日志组件

--- business-service/consume-project/consume-service/src/main/java/com/spring/cloud/controller/ConfigController.java
+++ business-service/consume-project/consume-service/src/main/java/com/spring/cloud/controller/ConfigController.java
@@ -1,9 +1,12 @@
 package com.spring.cloud.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/config")
 @RestController
 public class ConfigController {
-
+    private static final Logger log = LoggerFactory.getLogger(ConfigController.class);
 
     @Value("${test.hello}")
     private String hello;
@@ -30,5 +33,11 @@ public class ConfigController {
     @RequestMapping("/test/nacos")
     public String testNacos(){
         return hello;
+    }
+
+    @RequestMapping("/test/log/{msg}")
+    public String testLog(@PathVariable String msg){
+        log.info("测试log-spring-boot-starter，日志内容: {}", msg);
+        return msg;
     }
 }
